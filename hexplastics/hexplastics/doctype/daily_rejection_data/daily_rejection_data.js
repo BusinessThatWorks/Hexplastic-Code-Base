13e7a8d7d3532810ef1042e32951050997b3876c
clapgrowdev1 <clapgrowdev1@clapgrow.com>
2025-12-11 16:10:46 +0530

    feat: implement calculation of total rejection and rejection percentage in Daily Rejection Data

--- hexplastics/hexplastics/doctype/daily_rejection_data/daily_rejection_data.js
+++ hexplastics/hexplastics/doctype/daily_rejection_data/daily_rejection_data.js
@@ -1,8 +1,49 @@
 // Copyright (c) 2025, beetashoke chakraborty and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Daily Rejection Data", {
-// 	refresh(frm) {
+frappe.ui.form.on("Daily Rejection Data", {
+	refresh(frm) {
+		// Calculate on form refresh to populate existing values
+		frm.trigger("calculate_rejection");
+	},
 
-// 	},
-// });
+	calculate_rejection(frm) {
+		// Calculate total_rejection
+		const total_rejection =
+			(flt(frm.doc.box_rejected_by_die_punching) || 0) +
+			(flt(frm.doc.box_rejected_by_printing) || 0) +
+			(flt(frm.doc.box_rejected_by_bending) || 0) +
+			(flt(frm.doc.box_rejected_by_stepling) || 0);
+
+		frm.set_value("total_rejection", total_rejection);
+
+		// Calculate rejection_in_% = (total_rejection / total_box_checked) * 100
+		const total_box_checked = flt(frm.doc.total_box_checked) || 0;
+		if (total_box_checked > 0) {
+			const rejection_percentage = (total_rejection / total_box_checked) * 100;
+			frm.set_value("rejection_in_", rejection_percentage);
+		} else {
+			frm.set_value("rejection_in_", 0);
+		}
+	},
+
+	box_rejected_by_die_punching(frm) {
+		frm.trigger("calculate_rejection");
+	},
+
+	box_rejected_by_printing(frm) {
+		frm.trigger("calculate_rejection");
+	},
+
+	box_rejected_by_bending(frm) {
+		frm.trigger("calculate_rejection");
+	},
+
+	box_rejected_by_stepling(frm) {
+		frm.trigger("calculate_rejection");
+	},
+
+	total_box_checked(frm) {
+		frm.trigger("calculate_rejection");
+	},
+});
--- hexplastics/hexplastics/doctype/daily_rejection_data/daily_rejection_data.py
+++ hexplastics/hexplastics/doctype/daily_rejection_data/daily_rejection_data.py
@@ -1,9 +1,24 @@
 # Copyright (c) 2025, beetashoke chakraborty and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class DailyRejectionData(Document):
-	pass
+	def validate(self):
+		"""Calculate total_rejection and rejection_in_%"""
+		# Calculate total_rejection
+		total_rejection = (
+			(self.box_rejected_by_die_punching or 0) +
+			(self.box_rejected_by_printing or 0) +
+			(self.box_rejected_by_bending or 0) +
+			(self.box_rejected_by_stepling or 0)
+		)
+		self.total_rejection = total_rejection
+		
+		# Calculate rejection_in_% = (total_rejection / total_box_checked) * 100
+		if self.total_box_checked and self.total_box_checked > 0:
+			self.rejection_in_ = (total_rejection / self.total_box_checked) * 100
+		else:
+			self.rejection_in_ = 0
